ff1b12afd553308dbfbb6e51d49d83c95f0bbe9d
Seojkc <seojameskc007@gmail.com>
2026-04-01 09:48:23 -0400

    allow Vercel frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -96,7 +96,7 @@ app.add_middleware(
         "http://192.168.50.48:3000",
         "http://127.0.0.1:3000",
         "https://money-compass-bgx7ic46s-seojkcs-projects.vercel.app/",
-        "https://money-compass-navy.vercel.app/"
+        "https://money-compass-navy.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
